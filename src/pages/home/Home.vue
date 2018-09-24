da592bc27cbd12b9feba38376302eff603ff80d4
gong <783989066@qq.com>
2018-09-24 22:21:56 +0800

    vue项目初始化和一些配置

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+	  <div class="home">home</div>
+	  <router-link to="/list">列表页</router-link>  <!-- 跳转 -->
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home'  //组件名
+}
+</script>
+
+
+<style>
+	.home {
+		font-size: 50px;
+	}
+</style>
